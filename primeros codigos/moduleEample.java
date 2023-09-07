7aead8a42d07a5fa9c2dde04561dc69cc3aa128d
Juan Camilo Rodríguez Pineda <142944581+Juancamilo29x@users.noreply.github.com>
2023-09-07 17:04:59 -0500

    Create moduleEample.java

---
+++ primeros codigos/moduleEample.java
@@ -0,0 +1,11 @@
+public class Modulo {
+    public static void main(String[] args) {
+
+        int estudiantes = 26;
+
+
+        int sobrantes = estudiantes % 3;
+
+        System.out.println("Estudiantes sobrantes: " + sobrantes);
+    }
+}
