4f0f9c148175c5379f79f0937f96e52a1eafd17e
Juan Camilo Rodríguez Pineda <142944581+Juancamilo29x@users.noreply.github.com>
2023-09-07 17:10:05 -0500

    Create variables1.java

---
+++ primeros codigos/variables1.java
@@ -0,0 +1,8 @@
+public class Creador {
+    public static void main(String[] args) {
+        String nombre = "James Gosling";
+        int añoCreacion = 1995;      //El tipo de dato int permite valores entre -2,147,483,648 y 2,147,483,647, inclusive.
+        System.out.println(nombre);
+        System.out.println(añoCreacion);
+    }
+}
