a2d6dd2df94a3fa1c327fa874c43b30a8451f7e8
Juan Camilo Rodríguez Pineda <142944581+Juancamilo29x@users.noreply.github.com>
2023-09-07 16:59:01 -0500

    Create booleanExample.java

---
+++ primeros codigos/booleanExample.java
@@ -0,0 +1,6 @@
+public class Booleans {
+    public static void main(String[] args) {    
+        boolean losEnterosPuedenTenerDecimales = false;
+        System.out.println(losEnterosPuedenTenerDecimales);
+    }
+}
