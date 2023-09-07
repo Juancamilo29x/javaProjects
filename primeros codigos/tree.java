b3937254f45d2bdd622716924cd4e74fc6646242
Juan Camilo Rodríguez Pineda <142944581+Juancamilo29x@users.noreply.github.com>
2023-09-07 16:57:19 -0500

    Create tree.java

---
+++ primeros codigos/tree.java
@@ -0,0 +1,61 @@
+class Tree  
+{    
+    public static void main(String args[]) {  
+       System.out.println("Hey there, I’m Henry and I’m learning to code in Java!");
+       System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
+       System.out.println("                                  _______");
+       System.out.println("                           _,,ad8888888888bba,_");
+       System.out.println("                        ,ad88888I888888888888888ba,");
+       System.out.println("                     ,88888888I88888888888888888888a,");
+       System.out.println("                   ,d888888888I8888888888888888888888b,");
+       System.out.println("                  d88888PP"""" ""YY88888888888888888888b,");
+       System.out.println("                ,d88"'__,,--------,,,,.;ZZZY8888888888888,");
+       System.out.println("              ,8IIl'"                ;;l"ZZZIII8888888888,");
+       System.out.println("              ,I88l;'                  ;lZZZZZ888III8888888,");
+       System.out.println("             ,II88Zl;.                  ;llZZZZZ888888I888888,");
+       System.out.println("             ,II888Zl;.                .;;;;;lllZZZ888888I8888b");
+       System.out.println("           ,II8888Z;;                 `;;;;;''llZZ8888888I8888,");
+       System.out.println("           II88888Z;'                        .;lZZZ8888888I888b");
+       System.out.println("           II88888Z; _,aaa,      .,aaaaa,__.l;llZZZ88888888I888");
+       System.out.println("           II88888IZZ<'(@@>Z|  |ZZZ<'(@@>ZZZZ;;llZZ888888888I88I");
+       System.out.println("          ,II88888;   `""" ;|  |ZZ; `"""     ;;llZ8888888888I888");
+       System.out.println("          II888888l            `;;          .;llZZ8888888888I888,");
+       System.out.println("         ,II888888Z;           ;;;        .;;llZZZ8888888888I888I");
+       System.out.println("         III888888Zl;    ..,   `;;       ,;;lllZZZ88888888888I888");
+       System.out.println("         II88888888Z;;...;(_    _)      ,;;;llZZZZ88888888888I888,");
+       System.out.println("         II88888888Zl;;;;;' `--'Z;.   .,;;;;llZZZZ88888888888I888b");
+       System.out.println("         ]I888888888Z;;;;'   ";llllll;..;;;lllZZZZ88888888888I8888,");
+       System.out.println("         II888888888Zl.;;"Y88bd888P";;,..;lllZZZZZ88888888888I8888I");
+       System.out.println("         II8888888888Zl;.; `"PPP";;;,..;lllZZZZZZZ88888888888I88888");
+       System.out.println("         II888888888888Zl;;. `;;;l;;;;lllZZZZZZZZW88888888888I88888");
+       System.out.println("         `II8888888888888Zl;.    ,;;lllZZZZZZZZWMZ88888888888I88888");
+       System.out.println("          II8888888888888888ZbaalllZZZZZZZZZWWMZZZ8888888888I888888,");
+       System.out.println("          `II88888888888888888b"WWZZZZZWWWMMZZZZZZI888888888I888888b");
+       System.out.println("           `II88888888888888888;ZZMMMMMMZZZZZZZZllI888888888I8888888");
+       System.out.println("            `II8888888888888888 `;lZZZZZZZZZZZlllll888888888I8888888,");
+       System.out.println("             II8888888888888888, `;lllZZZZllllll;;.Y88888888I8888888b,");
+       System.out.println("             ,II8888888888888888b   .;;lllllll;;;.;..88888888I88888888b,");
+       System.out.println("            II888888888888888PZI;.  .`;;;.;;;..; ...88888888I8888888888,");
+       System.out.println("            II888888888888PZ;;';;.   ;. .;.  .;. .. Y8888888I88888888888b,");
+       System.out.println("            II888888888888PZ;;';;.   ;. .;.  .;. .. Y8888888I88888888888b,");
+       System.out.println("           ,II888888888PZ;;'                        `8888888I8888888888888b,");
+       System.out.println("           II888888888'                              888888I8888888888888888b");
+       System.out.println("          ,II888888888                              ,888888I88888888888888888");
+       System.out.println("         ,d88888888888                              d888888I8888888888ZZZZZZZ");
+       System.out.println("      ,ad888888888888I                              8888888I8888ZZZZZZZZZZZZZ");
+       System.out.println("    ,d888888888888888'                              888888IZZZZZZZZZZZZZZZZZZ");
+       System.out.println("  ,d888888888888P'8P'                               Y888ZZZZZZZZZZZZZZZZZZZZZ");
+       System.out.println(" ,8888888888888,  "                                 ,ZZZZZZZZZZZZZZZZZZZZZZZZ");
+       System.out.println("888888888888888888a,      _                    ,ZZZZZZZZZZZZZZZZZZZZ888888888");
+       System.out.println("888888888888888888888ba,_d'                  ,ZZZZZZZZZZZZZZZZZ88888888888888");
+       System.out.println("8888888888888888888888888888bbbaaa,,,______,ZZZZZZZZZZZZZZZ888888888888888888");
+       System.out.println("88888888888888888888888888888888888888888ZZZZZZZZZZZZZZZ888888888888888888888");
+       System.out.println("8888888888888888888888888888888888888888ZZZZZZZZZZZZZZ88888888888888888888888");
+       System.out.println("888888888888888888888888888888888888888ZZZZZZZZZZZZZZ888888888888888888888888");
+       System.out.println("8888888888888888888888888888888888888ZZZZZZZZZZZZZZ88888888888888888888888888");
+       System.out.println("88888888888888888888888888888888888ZZZZZZZZZZZZZZ8888888888888888888888888888");
+       System.out.println("8888888888888888888888888888888888ZZZZZZZZZZZZZZ88888888888888888   Juan   88");
+       System.out.println("88888888888888888888888888888888ZZZZZZZZZZZZZZ8888888888888888888  Camilo  88");
+       System.out.println("8888888888888888888888888888888ZZZZZZZZZZZZZZ88888888888888888888888888888888");
+   }  
+}  
