5ae6d00be4ee1fea4d4520ea0ef72dd8d65e8e5c
Juan Camilo Rodríguez Pineda <142944581+Juancamilo29x@users.noreply.github.com>
2023-09-07 17:03:46 -0500

    Create finalKeywordExample1.java

---
+++ primeros codigos/finalKeywordExample1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        final double pi = 3.14;
+        System.out.println(pi);
+        // La siguiente línea generará un error, ya que pi es una constante y no se puede cambiar.
+        // pi = 2 * pi;
+    }
+}
