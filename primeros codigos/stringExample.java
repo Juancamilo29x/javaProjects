66e8053401acaab30d92c14e868c2fe7fd6310d0
Juan Camilo Rodríguez Pineda <142944581+Juancamilo29x@users.noreply.github.com>
2023-09-07 17:09:17 -0500

    Create stringExample.java

---
+++ primeros codigos/stringExample.java
@@ -0,0 +1,6 @@
+public class Cancion {
+    public static void main(String[] args) {   
+        String letraInicial = "Ayer, todos mis problemas parecían tan lejanos";
+        System.out.println(letraInicial);
+    }
+}
